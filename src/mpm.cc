ade71ca70cfd3b936317b128d5a81152f3876add
RyotaHashimoto <ryotahashimoto@hiroshima-u.ac.jp>
2021-10-13 18:41:20 -0700

    Add MPMImplicit class

--- src/mpm.cc
+++ src/mpm.cc
@@ -5,6 +5,7 @@
 #include "mpm.h"
 #include "mpm_explicit.h"
 #include "mpm_explicit_twophase.h"
+#include "mpm_implicit.h"
 #include "mpm_implicit_linear.h"
 #include "mpm_semi_implicit_navierstokes.h"
 #include "mpm_semi_implicit_twophase.h"
@@ -17,6 +18,14 @@ static Register<mpm::MPM, mpm::MPMExplicit<2>, const std::shared_ptr<mpm::IO>&>
 // 3D Explicit MPM
 static Register<mpm::MPM, mpm::MPMExplicit<3>, const std::shared_ptr<mpm::IO>&>
     mpm_explicit_3d("MPMExplicit3D");
+
+// 2D Implicit MPM
+static Register<mpm::MPM, mpm::MPMImplicit<2>, const std::shared_ptr<mpm::IO>&>
+    mpm_implicit_2d("MPMImplicit2D");
+
+// 3D Implicit MPM
+static Register<mpm::MPM, mpm::MPMImplicit<3>, const std::shared_ptr<mpm::IO>&>
+    mpm_implicit_3d("MPMImplicit3D");
 
 // 2D Implicit Linear MPM
 static Register<mpm::MPM, mpm::MPMImplicitLinear<2>,

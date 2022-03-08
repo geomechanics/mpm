e3243d902ed4ec905fc0fd86085117baf7bd1746
RyotaHashimoto <ryotahashimoto@hiroshima-u.ac.jp>
2022-03-08 14:08:14 -0800

    Add files for ParticleFiniteStrain class

---
+++ include/particles/finite_strain/particle_finite_strain.h
@@ -0,0 +1,114 @@
+#ifndef MPM_PARTICLE_FINITE_STRAIN_H_
+#define MPM_PARTICLE_FINITE_STRAIN_H_
+
+#include <array>
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "logger.h"
+#include "particle.h"
+
+namespace mpm {
+
+//! ParticleFiniteStrain class
+//! \brief Class for the finite strain single-phase particle
+//! \tparam Tdim Dimension
+template <unsigned Tdim>
+class ParticleFiniteStrain : public mpm::Particle<Tdim> {
+ public:
+  //! Define a vector of size dimension
+  using VectorDim = Eigen::Matrix<double, Tdim, 1>;
+
+  //! Construct a particle using finite strain formulation with id and
+  //! coordinates \param[in] id Particle id \param[in] coord Coordinates of the
+  //! particles
+  ParticleFiniteStrain(Index id, const VectorDim& coord);
+
+  //! Construct a particle using finite strain formulation with id, coordinates
+  //! and status \param[in] id Particle id \param[in] coord coordinates of the
+  //! particle \param[in] status Particle status (active / inactive)
+  ParticleFiniteStrain(Index id, const VectorDim& coord, bool status);
+
+  //! Destructor
+  ~ParticleFiniteStrain() override{};
+
+  //! Delete copy constructor
+  ParticleFiniteStrain(const ParticleFiniteStrain<Tdim>&) = delete;
+
+  //! Delete assignment operator
+  ParticleFiniteStrain& operator=(const ParticleFiniteStrain<Tdim>&) = delete;
+
+  //! Map internal force
+  inline void map_internal_force() noexcept override;
+
+  //! Type of particle
+  std::string type() const override { return (Tdim == 2) ? "P2DFS" : "P3DFS"; }
+
+  /**
+   * \defgroup Implicit Functions dealing with implicit MPM
+   */
+  /**@{*/
+  //! Compute B matrix of a particle, based on local coordinates
+  //! \ingroup Implicit
+  inline Eigen::MatrixXd compute_bmatrix() noexcept override;
+  /**@}*/
+
+ protected:
+  //! Compute strain rate
+  //! \ingroup Implicit
+  //! \param[in] dn_dx The spatial gradient of shape function
+  //! \param[in] phase Index to indicate phase
+  //! \retval strain rate at particle inside a cell
+  inline Eigen::Matrix<double, 6, 1> compute_strain_rate(
+      const Eigen::MatrixXd& dn_dx, unsigned phase) noexcept override;
+
+  /**
+   * \defgroup Implicit Functions dealing with implicit MPM
+   */
+  /**@{*/
+  //! Compute strain increment
+  //! \ingroup Implicit
+  //! \param[in] dn_dx The spatial gradient of shape function
+  //! \param[in] phase Index to indicate phase
+  //! \retval strain increment at particle inside a cell
+  inline Eigen::Matrix<double, 6, 1> compute_strain_increment(
+      const Eigen::MatrixXd& dn_dx, unsigned phase) noexcept override;
+  /**@}*/
+
+ protected:
+  //! Nodes
+  using ParticleBase<Tdim>::nodes_;
+  //! Volume
+  using Particle<Tdim>::volume_;
+  //! Stresses
+  using Particle<Tdim>::stress_;
+  //! Strains
+  using Particle<Tdim>::strain_;
+  //! dvolumetric strain
+  using Particle<Tdim>::dvolumetric_strain_;
+  //! Volumetric strain at centroid
+  using Particle<Tdim>::volumetric_strain_centroid_;
+  //! Strain rate
+  using Particle<Tdim>::strain_rate_;
+  //! dstrains
+  using Particle<Tdim>::dstrain_;
+  //! Velocity
+  using Particle<Tdim>::velocity_;
+  //! Displacement
+  using Particle<Tdim>::displacement_;
+  //! dN/dX
+  using Particle<Tdim>::dn_dx_;
+  //! dN/dX at cell centroid
+  using Particle<Tdim>::dn_dx_centroid_;
+
+  //! Logger
+  std::unique_ptr<spdlog::logger> console_;
+
+};  // ParticleFiniteStrain class
+}  // namespace mpm
+
+#include "particle_finite_strain.tcc"
+
+#endif  // MPM_PARTICLE_FINITE_STRAIN_H__

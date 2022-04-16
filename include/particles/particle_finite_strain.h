82611c83a0a61b88eb8a5061e6a50f93c699f600
RyotaHashimoto <ryotahashimoto@hiroshima-u.ac.jp>
2022-04-16 08:30:58 -0700

    Add test for ParticleFiniteStrain in explicit

--- include/particles/particle_finite_strain.h
+++ include/particles/particle_finite_strain.h
@@ -54,6 +54,11 @@ class ParticleFiniteStrain : public mpm::Particle<Tdim> {
     return strain;
   }
 
+  //! Return deformation gradient increment of the particle
+  Eigen::Matrix<double, 3, 3> deformation_gradient_increment() const {
+    return deformation_gradient_increment_;
+  }
+
   //! Update volume based on deformation gradient increment
   //! Note: Volume is updated in compute_strain_newmark() every N-R iteration
   void update_volume() noexcept override{};

27f872d5b71cd4c8b67b546fb9d0c7782b09e186
bodhinandach <bodhinandach@gmail.com>
2022-09-01 15:45:12 -0700

    Add arm64 compatibility

--- external/catch.hpp
+++ external/catch.hpp
@@ -2141,7 +2141,11 @@ namespace Catch{
                 __asm__("li r0, 20\nsc\nnop\nli r0, 37\nli r4, 2\nsc\nnop\n" \
                 : : : "memory","r0","r3","r4" ) /* NOLINT */
     #else
-        #define CATCH_TRAP() __asm__("int $3\n" : : /* NOLINT */ )
+        #if defined(__i386__) || defined(__x86_64__)
+            #define CATCH_TRAP() __asm__("int $3\n" : : ) /* NOLINT */
+        #elif defined(__aarch64__)
+            #define CATCH_TRAP()  __asm__(".inst 0xd4200000")
+        #endif
     #endif
 
 #elif defined(CATCH_PLATFORM_LINUX)

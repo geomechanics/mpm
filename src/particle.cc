19e59cee56bc37753b4fca28fa50e491cc46b5e0
Nanda <bodhinandach@gmail.com>
2023-03-17 15:51:27 -0700

    :fire: Add new class of PML particle

--- src/particle.cc
+++ src/particle.cc
@@ -4,6 +4,7 @@
 #include "particle_bbar.h"
 #include "particle_finite_strain.h"
 #include "particle_fluid.h"
+#include "particle_pml.h"
 #include "particle_twophase.h"
 
 namespace mpm {
@@ -11,11 +12,11 @@ namespace mpm {
 std::map<std::string, int> ParticleType = {
     {"P2D", 0},       {"P3D", 1},       {"P2DFLUID", 2}, {"P3DFLUID", 3},
     {"P2D2PHASE", 4}, {"P3D2PHASE", 5}, {"P2DBBAR", 6},  {"P3DBBAR", 7},
-    {"P2DFS", 8},     {"P3DFS", 9}};
+    {"P2DFS", 8},     {"P3DFS", 9},     {"P2DPML", 10},  {"P3DPML", 11}};
 std::map<int, std::string> ParticleTypeName = {
     {0, "P2D"},       {1, "P3D"},       {2, "P2DFLUID"}, {3, "P3DFLUID"},
     {4, "P2D2PHASE"}, {5, "P3D2PHASE"}, {6, "P2DBBAR"},  {7, "P3DBBAR"},
-    {8, "P2DFS"},     {9, "P3DFS"}};
+    {8, "P2DFS"},     {9, "P3DFS"},     {10, "P2DPML"},  {11, "P3DPML"}};
 std::map<std::string, std::string> ParticlePODTypeName = {
     {"P2D", "particles"},
     {"P3D", "particles"},
@@ -26,7 +27,9 @@ std::map<std::string, std::string> ParticlePODTypeName = {
     {"P2DBBAR", "particles"},
     {"P3DBBAR", "particles"},
     {"P2DFS", "particles"},
-    {"P3DFS", "particles"}};
+    {"P3DFS", "particles"},
+    {"P2DPML", "particles"},
+    {"P3DPML", "particles"}};
 }  // namespace mpm
 
 // Particle2D (2 Dim)
@@ -78,3 +81,13 @@ static Register<mpm::ParticleBase<2>, mpm::ParticleFiniteStrain<2>, mpm::Index,
 static Register<mpm::ParticleBase<3>, mpm::ParticleFiniteStrain<3>, mpm::Index,
                 const Eigen::Matrix<double, 3, 1>&>
     particle3dfinitestrain("P3DFS");
+
+// Particle2D with perfectly-matched layer formulation (2 Dim)
+static Register<mpm::ParticleBase<2>, mpm::ParticlePML<2>, mpm::Index,
+                const Eigen::Matrix<double, 2, 1>&>
+    particle2dperfectlymatchedlayer("P2DPML");
+
+// Particle3D with perfectly-matched layer formulation (3 Dim)
+static Register<mpm::ParticleBase<3>, mpm::ParticlePML<3>, mpm::Index,
+                const Eigen::Matrix<double, 3, 1>&>
+    particle3dperfectlymatchedlayer("P3DPML");

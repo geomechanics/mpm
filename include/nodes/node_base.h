062fb0c354b4da771627b41cbd9a0c7e867c7f5e
Connor <cgeudeker@berkeley.edu>
2026-04-25 18:23:12 -0700

    Manual Clang fix part 2.

--- include/nodes/node_base.h
+++ include/nodes/node_base.h
@@ -39,7 +39,7 @@ class NodeBase {
   // Constructor with id and coordinates
   //! \param[in] id assign as the id_ of the node
   //! \param[in] coords coordinates of the node
-  NodeBase(mpm::Index id, const VectorDim& coords){};
+  NodeBase(mpm::Index id, const VectorDim& coords) {};
 
   //! Destructor
   virtual ~NodeBase() {};
